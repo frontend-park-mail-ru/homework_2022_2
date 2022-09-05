93ed635025e66a117cb796f7ca9b45e956447b78
Andeo1812 <alexanforall@gmail.com>
2022-09-05 15:52:15 +0300

    add exception

--- source/zip.js
+++ source/zip.js
@@ -1,9 +1,14 @@
 'use strict';
 
+
 const zip = (...objects) => {
     let result = {};
 
     for (let i = 0, length = objects.length; i < length; ++i) {
+        if (typeof (objects[i]) != "object") {
+            throw new SyntaxError("Arguments must be objects");
+        }
+
         for (let key in objects[i]) {
             if (!result.hasOwnProperty(key)) {
                 result[key] = objects[i][key];
