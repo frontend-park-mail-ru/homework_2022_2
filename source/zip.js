2f280d1ef1558333f5b9deaf3f9eecb0b17472e5
Andeo1812 <alexanforall@gmail.com>
2022-09-05 15:39:03 +0300

    miniref

--- source/zip.js
+++ source/zip.js
@@ -5,7 +5,6 @@ const zip = (...objects) => {
 
     for (let i = 0, length = objects.length; i < length; ++i) {
         for (let key in objects[i]) {
-
             if (!result.hasOwnProperty(key)) {
                 result[key] = objects[i][key];
             }
