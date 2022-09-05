e7a0e5df5aa53dcf45f2bbcf35ee0596eb722d8f
Vladislav <sdvlad75@gmail.com>
2022-09-05 22:57:28 +0300

    fixes

--- source/letters.js
+++ source/letters.js
@@ -1,105 +1,70 @@
 'use strict';
 
 /**
- * Проверка, состоит ли строка из одного повторяющегося символа
- * @param {string} str - строка
- * @returns {boolean}
- */
-const isStrConsistsOfOneChar = (str) => {
-    let temp = str[0]
-
-    for (let i in str) {
-        if (i != 0 && str[i] !== temp) return false;
-    }
-
-    return true;
-}
-
-/**
- * Проверка, встретится ли символ на месте valueIndex еще раз в строке
- * @param {number} valueIndex 
- * @param {string} str 
- * @returns {boolean} 
- */
-const isNextValueInStr = (valueIndex, str) => {
-    if (str.lastIndexOf(str[valueIndex]) != valueIndex) {
-        return true;
-    }
-
-    return false;
-}
-
-/**
- * Удалит все повторяющиеся символы кроме первого
- * @param {string} str 
- * @returns {string}
- */
-const saveFirstLetters = (str) => {
-    let res = []
-
-    for (let i in str) {
-        if (!res.includes(str[i])) {
-            res.push(str[i])
-        }
-    }
-
-    return res.join('')
-}
-
-/**
- * Удалит все повторяющиеся символы кроме последнего
- * @param {string} str 
- * @returns {string}
- */
-const saveLastLetters = (str) => {
-    let res = []
-
-    for (let i in str) {
-        if (!isNextValueInStr(i, str)) {
-            res.push(str[i])
-        }
-    }
-
-    console.log(str, res)
-
-    return res.join('')
-}
-
-/**
- * Удалит все повторяющиеся символы
- * @param {string} str - исходная строка
- * @returns {string}
- */
-const deleteAllRepeatedSymbols = (str) => {
-    let res = []
-
-    for (let i in str) {
-        if (str.lastIndexOf(str[i]) == i && str.indexOf(str[i]) == i) {
-            res.push(str[i])
-        }
-    }
-
-    return res.join('')
-}
-
-/**
- * 
- * @param {string} str - Исходная строка 
+ *
+ * @param {string} str - Исходная строка
  * @param {boolean} flag
  * @returns {string} - Результат операции удаления повторяющихся символов
  */
-const letters = (str, flag) => { 
-    if (typeof str !== "string") throw new Error("Некорректный тип данных")
+const letters = (str, flag) => {
+    /**
+     * Проверка, состоит ли строка из одного повторяющегося символа
+     * @param {string} str - исходная строка
+     * @returns {boolean} - результат проверки
+     */
+    const isStrConsistsOfOneChar = (str) => {
+        const temp = str[0];
 
-    if (str === '' || isStrConsistsOfOneChar(str.split(''))) return ''
+        if (str.some((elem) => elem !== temp)) return false;
+        return true;
+    };
 
-    if (flag === undefined) {
-        return deleteAllRepeatedSymbols(str.split(''))
-    }
+    /**
+     * Удалит все повторяющиеся символы кроме первого
+     * @param {string} str - исходная строка
+     * @returns {string}
+     */
+    const saveFirstLetters = (str) => {
+        const res = str.split('').filter((value, index, self) => {
+            return self.indexOf(value) === index;
+        });
 
-    if (flag) {
-        return saveFirstLetters(str.split(''))
-    }
+        return res.join('');
+    };
 
-    return saveLastLetters(str.split(''));
-}
+    /**
+     * Удалит все повторяющиеся символы кроме последнего
+     * @param {string} str - исходная строка
+     * @returns {string}
+     */
+    const saveLastLetters = (str) => {
+        const res = str.split('').filter((value, index, self) => {
+            return self.lastIndexOf(value) === index;
+        });
+
+        return res.join('');
+    };
+
+    /**
+     * Удалит все повторяющиеся символы
+     * @param {string} str - исходная строка
+     * @returns {string}
+     */
+    const saveOnlyUniqueLetters = (str) => {
+        const res = str.split('').filter((value, index, self) => {
+            return self.lastIndexOf(value) === self.indexOf(value);
+        });
+
+        return res.join('');
+    };
+
+    if (typeof str !== 'string') throw new Error('Некорректный тип данных');
+    if (str === '' || isStrConsistsOfOneChar(str.split(''))) return '';
+    if (flag === undefined) return saveOnlyUniqueLetters(str);
+
+    let res = '';
+
+    flag ? (res = saveFirstLetters(str)) : (res = saveLastLetters(str));
+
+    return res;
+};
