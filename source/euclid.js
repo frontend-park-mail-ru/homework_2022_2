237d14bfddf1c99d49fceba44ae24cd94011306b
Leonard Pak <leopak2000@gmail.com>
2022-09-06 11:54:34 +0300

    Remake checking

--- source/euclid.js
+++ source/euclid.js
@@ -8,6 +8,29 @@
 const isNaturalNumber = num => (num > 0) && (Math.trunc(num) === num);
 
 /**
+ * Make function for for checking if any argument is a natural number
+ * @param {number => bool} checkNatNum - the function returning true if argument is 
+ * natural number
+ * @returns {any => number} function for checking if any argument is a natural number
+ * Returns the equivalent number 
+ */
+function makeIsNatureNumber(checkNatNum) {
+  let errStrNum = ' is not a number';
+  let errStrNatNum = ' is not a natural number';
+
+  return function (num) {
+    let numLocal = +num;
+    if (!Number.isFinite(numLocal)) {
+      throw TypeError(`${num}` + errStrNum)
+    }
+    if (!checkNatNum(numLocal)) {
+      throw RangeError(`${num}` + errStrNatNum)
+    }
+    return numLocal
+  }
+}
+
+/**
  * Find greatest common divisor using Euclid`s algorithm
  * @param {number} num1  - first natural number
  * @param {number} num2 - second natural number
@@ -16,34 +39,23 @@ const isNaturalNumber = num => (num > 0) && (Math.trunc(num) === num);
  * @throws {RangeError} Arguments is not a natural number
  */
 function euclidRaw(num1, num2) {
-  let localNum1 = +num1;
-  let localNum2 = +num2;
-  if (!Number.isFinite(localNum1)) {
-    throw TypeError(`${num1} is not a number`);
-  }
-  if (!Number.isFinite(localNum2)) {
-    throw TypeError(`${num2} is not a number`);
-  }
-  if (!isNaturalNumber(localNum1)) {
-    throw RangeError(`${num1} is not a natural number`);
-  }
-  if (!isNaturalNumber(localNum2)) {
-    throw RangeError(`${num2} is not a natural number`);
-  }
+  let checkerNatNum = makeIsNatureNumber(isNaturalNumber);
+  num1 = checkerNatNum(num1);
+  num2 = checkerNatNum(num2);
 
-  while (localNum1 !== 0 && localNum2 !== 0) {
-    if (localNum1 > localNum2) {
-      localNum1 %= localNum2;
+  while (num1 !== 0 && num2 !== 0) {
+    if (num1 > num2) {
+      num1 %= num2;
     } else {
-      localNum2 %= localNum1;
+      num2 %= num1;
     }
   }
-  return (localNum1 == 0) ? localNum2 : localNum1;
+  return (num1 == 0) ? num2 : num1;
 }
 
 /**
  * Find greatest common division in an array of number using Euclid`s algorithm
- * @param  {number} numArr - array of natural number
+ * @param  {...number} numArr - array of natural number
  * @returns {number} greatest common division
  */
 const euclid = (...numArr) => numArr.reduce(euclidRaw, numArr[0]);
