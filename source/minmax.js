e52bc2dec239cf41639aeb9fe42a89611ef93ad7
DK <dkostev68@gmail.com>
2022-09-06 11:42:12 +0300

    Fix style

--- source/minmax.js
+++ source/minmax.js
@@ -1,11 +1,15 @@
 'use strict'
-
-const minmax = (string) => {
-    const regularExpression = /(-?(\d+)?\.?\d+e?-?(\d+)?)|(-?Infinity)/g;
-    const numbers = string.match(regularExpression);
-    if (numbers === null) {
-        return [ undefined, undefined ];
+/**
+ *  Функция minmax находит минимальное и максимальное значения в строке.
+ *  Возвращает массив, в котором первый элемент — это минимальное число в строке, второй элемент — максимальное.
+ * 
+ * Функция принимает 1 параметр stringWithNumbers типа string
+ */
+const minmax = (stringWithNumbers) => {
+    if (typeof stringWithNumbers !== 'string') {
+        throw new Error('INVALID_ARGUMENT_TYPE');
     }
-    const arrayOfNumbers = numbers.map(v => +v);
-    return( [Math.min(...arrayOfNumbers), Math.max(...arrayOfNumbers)]);
+    const regularExpression = /(-?(\d+)?\.?\d+e?-?(\d+)?)|(-?Infinity)/g;
+    const numbers = stringWithNumbers.match(regularExpression)?.map(v => +v);
+    return (numbers === undefined ? [undefined, undefined] : [Math.min(...numbers), Math.max(...numbers)]);
 };
--- tests/minmax.js
+++ tests/minmax.js
@@ -4,6 +4,7 @@ QUnit.module('Тестируем функцию minmax', function () {
 	QUnit.test('minmax работает правильно на строках без чисел', function (assert) {
 		assert.deepEqual(minmax(''), [ undefined, undefined ], 'Особый случай, когда в строке нет чисел');
 		assert.deepEqual(minmax('мама мыла раму'), [ undefined, undefined ]);
+		assert.deepEqual(minmax('/!"№;%:?*()[]{},.`~"'), [ undefined, undefined ]);
 	});
 
 	QUnit.test('minmax правильно парсит отдельные числа', function (assert) {
@@ -19,6 +20,7 @@ QUnit.module('Тестируем функцию minmax', function () {
 		assert.deepEqual(minmax('1e5'), [ 1e5, 1e5 ]);
 		assert.deepEqual(minmax('-1e-5'), [ -1e-5, -1e-5 ]);
 		assert.deepEqual(minmax('-.1e-5'), [ -.1e-5, -.1e-5 ]);
+		assert.deepEqual(minmax('-1.1e-5'), [ -1.1e-5, -1.1e-5 ]);
 	});
 
 	QUnit.test('minmax правильно парсит несколько чисел', function (assert) {
@@ -27,14 +29,11 @@ QUnit.module('Тестируем функцию minmax', function () {
 		assert.deepEqual(minmax('1 2 3 4'), [ 1, 4 ]);
 		assert.deepEqual(minmax('-Infinity -1 0 1 Infinity'), [ -Infinity, Infinity ]);
 		assert.deepEqual(minmax('-.01 0 .01'), [ -.01, .01 ]);
+		assert.deepEqual(minmax('1 2 3 Infinity'), [ 1, Infinity ]);
 	});
 
 	QUnit.test('minmax игнорирует обычный текст', function (assert) {
 		assert.deepEqual(minmax('1, -5.8 или 10, хотя 34 + -5.3 и 73'), [ -5.8, 73 ]);
-	});
-	
-	QUnit.test('Парочка дополнительных тестов :)', function (assert) {
-		assert.deepEqual(minmax('1 2 3 4 5'), [ 1, 5]);
-		assert.deepEqual(minmax('A5B1C4D28G-100'), [ -100, 28 ]);
+		assert.deepEqual(minmax('1 and 2 is too simple for 10!'), [ 1, 10 ]);
 	});
 });
