c42cc55aca2485ba2703e12d8684648033bf1ce2
lonkidely <inkandies.social@gmail.com>
2022-09-06 12:53:07 +0300

    Another implementation of plain function

--- source/plain.js
+++ source/plain.js
@@ -5,4 +5,19 @@
  * @param {Array} array - Массив массивов с элементами произвольных типов
  * @returns {Array} Результирующий массив, в котором все элементы на одном уровне вложенности
  */
-const plain = (array) => array.flat(Infinity);
+const plain = (array) => {
+    const stack = [...array];
+    const result_array = [];
+
+    while (stack.length) {
+        const current_element = stack.pop();
+        
+        if (Array.isArray(current_element)) {
+            stack.push(...current_element);
+        } else {
+            result_array.push(current_element);
+        }
+    }
+
+    return result_array.reverse();
+}
