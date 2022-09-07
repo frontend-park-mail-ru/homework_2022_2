228bde90448acdcb621bc627971dcd89fc05ce3a
Liza1040 <maxsimova2002eliza@yandex.ru>
2022-09-07 17:02:03 +0300

    add validation of input parameters

--- source/plainify.js
+++ source/plainify.js
@@ -6,17 +6,30 @@
  * @param {Object} nested - Объект с вложенными свойствами
  * @param {string} propertyName - Имя свойства
  * @returns {Object} - plain-объект
+ * @throws {TypeError} - Если аргумент имеет неверный тип
+ * @throws {Error} - Если аргумент является пустым объектом
  * @example
  * // returns {'foo': 'bar', 'baz': 42}
  * plainify({foo: 'bar', baz: 42})
  */
 const plainify = (nested, propertyName = '') => {
+    if (typeof nested != 'object') {
+        throw new TypeError('Invalid data was passed to the function');
+    }
+
+    if (nested === null) {
+        throw new TypeError('A null argument was passed to the function');
+    }
+
+    if (Object.keys(nested).length == 0) {
+        throw new Error('Function argument must not be an empty object')
+    }
+
     const propertyArray = Object.entries(nested); //получаем массив свойств объекта
 
     // функция обратного вызова
     const callbackFunction = (plainObj, [key, value]) => {
         const newPropertyName = `${propertyName}${propertyName ? '.' : ''}${key}`; // добавление ключа к имени свойства
-        alert(typeof propertyName);
         return Object.assign(plainObj, value instanceof Object ? plainify(value, newPropertyName) : {[newPropertyName]: value}); // копирование свойств в целевой объект
     }
 
