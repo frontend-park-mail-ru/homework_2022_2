f39c929c1613829a2f070594e3db3b01fcb61abb
DK <dkostev68@gmail.com>
2022-09-07 21:18:29 +0300

    Style update

--- source/minmax.js
+++ source/minmax.js
@@ -3,13 +3,15 @@
  *  Функция minmax находит минимальное и максимальное значения в строке.
  *  Возвращает массив, в котором первый элемент — это минимальное число в строке, второй элемент — максимальное.
  * 
- * Функция принимает 1 параметр stringWithNumbers типа string
+ * @param {string} stringWithNumbers - строка содержащаяя числа.
+ * @return {number[]} массив из двух чисел (минимальное и максимальное).
+ * @throws {INVALID_ARGUMENT_TYPE} - выбрасывается ошибка, если аргумент stringWithNumbers не типа string.
  */
 const minmax = (stringWithNumbers) => {
     if (typeof stringWithNumbers !== 'string') {
         throw new Error('INVALID_ARGUMENT_TYPE');
     }
     const regularExpression = /(-?(\d+)?\.?\d+e?-?(\d+)?)|(-?Infinity)/g;
-    const numbers = stringWithNumbers.match(regularExpression)?.map(v => +v);
-    return (numbers === undefined ? [undefined, undefined] : [Math.min(...numbers), Math.max(...numbers)]);
+    const numbers = stringWithNumbers.match(regularExpression)?.map(v => Number(v));
+    return (numbers ? [Math.min(...numbers), Math.max(...numbers)] : [undefined, undefined]);
 };
