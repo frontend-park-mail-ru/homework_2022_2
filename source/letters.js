f46e990b5b0a0f9a231f1e063c4347158eccfad6
FackOff25 <kozvladimir@bk.ru>
2022-09-07 12:47:28 +0300

    Initial

---
+++ source/letters.js
@@ -0,0 +1,3 @@
+//for all deleting just go through line remembering last letter and delete
+
+//for deleting all except one make struct of letter and its position and put it in hash table without duplicates. After it sort by position and make a string
