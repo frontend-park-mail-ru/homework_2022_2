ac3d1b6f237f2a33abda16120afbdcfbb30e99e1
zenehu <zeronethunter2001@gmail.com>
2022-09-08 21:06:28 +0300

    Regex change of first letter was fixed

--- source/sort.js
+++ source/sort.js
@@ -15,7 +15,7 @@ const sort = (str) => {
     const words = str.toLowerCase().split(' ');
     words.forEach((word, index) => {
             word = word.split('').sort(collator.compare).join('');
-            words[index] = word.replace(/^./, word[0].toUpperCase());
+            words[index] = word[0].toUpperCase() + word.slice(1);
         }
     )
     return words.sort(collator.compare).join(' ')
