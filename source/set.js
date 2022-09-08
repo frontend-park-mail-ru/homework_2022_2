6d443e2b8ec9e9ca153ce9a677a83511e9b471aa
yura <yura11011@yandex.com>
2022-09-08 22:12:47 +0300

    some more fixes

--- source/set.js
+++ source/set.js
@@ -10,27 +10,26 @@
  * @throws {TypeError} - выбрасывает исключение при некорректных аргументах
  */
 const set = (obj, path, val) => {
-        if (typeof path != "string" || typeof obj != "object" || typeof val == "undefined") {
-            throw TypeError("Invalid Argument")
-        }
-        if (path.indexOf(".") === -1) {
-            throw TypeError("Incorrect Path")
-        }
-        const keys = path.split('.');
-        keys.reduce(function (object, key, index, array) {
-            if (index === array.length - 1) {
-                object[key] = val
-                return object
-            }
-            if (key === "") {
-                return object
-            }
-            if (!(key in object)) {
-                object[key] = {}
-            }
-            object = object[key]
-            return object
-        }, obj)
-        return obj
+    if (typeof path !== "string" || typeof obj !== "object" || typeof val === "undefined") {
+        throw TypeError("Invalid Argument");
     }
-;
+    if (path.indexOf(".") === -1) {
+        throw TypeError("Incorrect Path");
+    }
+    const keys = path.split('.');
+    keys.reduce((object, key, index, array) => {
+        if (index === array.length - 1) {
+            object[key] = val;
+            return object;
+        }
+        if (key === "") {
+            return object;
+        }
+        if (!(key in object)) {
+            object[key] = {};
+        }
+        object = object[key];
+        return object;
+    }, obj);
+    return obj;
+};
