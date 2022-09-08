f79b411a3b117cfb6b756bc1456d4fd63db3b6f1
FackOff25 <kozvladimir@bk.ru>
2022-09-08 13:14:48 +0300

    added validation

--- source/letters.js
+++ source/letters.js
@@ -1,37 +1,41 @@
+"use strict";
+
 /**
-* Delete all not unique symbols in string if there is no boolean arguement
+* Delete all not unique symbols in string if there is no boolean argument
 * or delete all repeats if there is one
 * @param {string} - string to delete repeats
 * @param {bool} - not necessary; if provided define which enter will remain: if 
 * true then first one, if false then last one
 * @return {string} string without repeats
-* @example
+* @throws {TypeError} throws exception 'first argument must be string', if str
 * letters("irritating", true) returns irtang
 */
 
-function letters(string, bool = null){
+let letters = (lettersString, bool = null) => {
+    if (typeof lettersString != "string") throw new TypeError('first argument must be string');
+    if (typeof bool != "boolean" && bool != null) throw new TypeError('second argument must be string or absent');
     if (bool === null){
         let set = new Set();
         let extraSet = new Set();
 
-        for (char of string){
+        for (let char of lettersString){
             if (set.has(char)) extraSet.add(char);
             else set.add(char);
         }
-        for (item of extraSet){
+        for (let item of extraSet){
             set.delete(item);
         }
 
         return Array.from(set).join('');
     }
 
-    const start = bool ? 0 : string.length - 1;
-    const end = bool ? string.length : -1;
+    const start = bool ? 0 : lettersString.length - 1;
+    const end = bool ? lettersString.length : -1;
     const step = bool ? 1 : -1;
     
     let set = new Set();
     for (let i = start; i != end; i += step){
-        set.add(string[i]);
+        set.add(lettersString[i]);
     }
 
     let arr = Array.from(set);
