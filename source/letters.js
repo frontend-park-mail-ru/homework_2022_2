276d4dea0df0483220768bf9a4d7075ec7a2cf66
FackOff25 <kozvladimir@bk.ru>
2022-09-09 14:45:26 +0300

    fixes

--- source/letters.js
+++ source/letters.js
@@ -3,6 +3,7 @@
 /**
 * Delete all not unique symbols in string if there is no boolean argument
 * or delete all repeats if there is one
+*
 * @param {string} lettersString - string to delete repeats
 * @param {boolean} [mode] - if provided define which enter will remain: if
 * true then first one, if false then last one
@@ -11,10 +12,14 @@
 * @throws {TypeError} throws exception 'second argument must be string or absent', if provided mode is invalid
 * letters("irritating", true) returns irtang
 */
-
 const letters = (lettersString, mode = null) => {
-    if (!(typeof lettersString === 'string' || lettersString instanceof String)) throw new TypeError('first argument must be string');
-    if (typeof mode !== 'boolean' && mode !== null) throw new TypeError('second argument must be string or absent');
+    if (!(typeof lettersString === 'string' || lettersString instanceof String)) {
+        throw new TypeError('first argument must be string');
+    };
+
+    if (typeof mode !== 'boolean' && mode !== null) {
+        throw new TypeError('second argument must be string or absent');
+    };
 
     if (mode === null){
         const set = new Set();
@@ -24,14 +29,15 @@ const letters = (lettersString, mode = null) => {
             if (set.has(char)) extraSet.add(char);
             else set.add(char);
         }
+
         for (let item of extraSet){
             set.delete(item);
         }
 
-        return Array.from(set).join('');
+        return [...set].join('');
     }
     //if mode is true put string in set in direct order if false put in reverse order
-    const set = mode ? new Set(lettersString) : new Set(lettersString.split('').reverse().join(''));
+    const set = new Set(mode ? lettersString : [...lettersString].reverse());
 
     return (mode ? [...set] : [...set].reverse()).join('');
 }
