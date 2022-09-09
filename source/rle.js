c3b1bb87f0fba5a742b1e873b59de1b4b2cb1128
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2022-09-09 11:37:44 +0300

    fixed errors handling

--- source/rle.js
+++ source/rle.js
@@ -1,23 +1,27 @@
 'use strict';
 
 /**
- * реализация RLE сжатия без дополнитеных условий
+ * реализация RLE сжатия без дополнительных условий
  * @param {string} str - исходная строка.
+ * @throws {TypeError} - Некорректный тип аргумета
+ * @throws {Error} - Некорректная строка
+ * @example 
+ * //returns 'A3B'
+ * rle('AAAB');
  * @returns {string} ansString - RLE сжатая строка.
  */
-
 const rle = (str) => {
   let ansString = "";
   let letterCounter = 1;
-  if (!str) {
-    return "Incorrect object";
+  if (typeof str !== 'string') {
+    throw new TypeError("Incorrect object");
   }
   for (let i = 0; i < str.length; i++) {
     if (str[i] < 'A' || str[i] > 'Z') {
-        return "Incorrect string";
+      throw new Error("Incorrect string");
     }
     if (str[i] != str[i - 1]) {
-      if (letterCounter != 1) {
+      if (letterCounter !== 1) {
         ansString += letterCounter;
       }
       letterCounter = 1;
@@ -26,7 +30,7 @@ const rle = (str) => {
       letterCounter++;
     }
   }
-  if (letterCounter != 1) {
+  if (letterCounter !== 1) {
     ansString += letterCounter;
   }
   return ansString;
--- tests/rle.js
+++ tests/rle.js
@@ -11,9 +11,12 @@ QUnit.module('Тестируем функцию rle', function () {
 		assert.strictEqual(rle('A'), 'A');
 		assert.strictEqual(rle('ABCABCABC'), 'ABCABCABC');
 		assert.strictEqual(rle('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ'), 'Z36');
-		assert.strictEqual(rle('+'), 'Incorrect string');
-		assert.strictEqual(rle('AAAFSD3W'), 'Incorrect string');
-		assert.strictEqual(rle('a'), 'Incorrect string');
-		assert.strictEqual(rle(null), 'Incorrect object');
+		assert.throws(() => rle('+'), Error, 'Incorrect string');
+		assert.throws(() => rle('AAAFSD3W'), Error, 'Incorrect string');
+		assert.throws(() => rle('a'), Error, 'Incorrect string');
+		assert.throws(() => rle(null), TypeError, 'Incorrect object');
+		assert.throws(() => rle(Infinity), TypeError, 'Incorrect object');
+		assert.throws(() => rle(NaN), TypeError, 'Incorrect object');
+		assert.throws(() => rle([1,2,5,2]), TypeError, 'Incorrect object');
 	});
 });
