9f1b1128781390d0d66e719ea8adc3f1b2386cc6
aralary <makarov.0205@mail.ru>
2022-09-11 23:11:13 +0300

    + =>func + ternar operator + use strict - comments

--- source/format.js
+++ source/format.js
@@ -1,3 +1,5 @@
+"use strict"
+
 function maxColWidth(numbers, col, colsNumber) {
     let res = 0;
     for (let i = col; i < numbers.length; i += colsNumber) {
@@ -8,7 +10,7 @@ function maxColWidth(numbers, col, colsNumber) {
     return res;
 }
 
-function format(numbers, cols) {
+let format = (numbers, cols) => {
     let res = "";
     for (let i = 0; i < numbers.length; i += cols) {
         for (let j = 0; (j < cols) && (i + j < numbers.length); ++j) {
@@ -19,44 +21,9 @@ function format(numbers, cols) {
                 spaces += " ";
             }
             res += (spaces + numbers[i + j]);
-            if (j < cols - 1 && numbers[i + j] !== numbers[numbers.length - 1]) {
-                res += ' ';
-            } else if (j === cols - 1 && numbers[i + j] !== numbers[numbers.length - 1]) {
-                res += '\n';
-            }
+            (j < cols - 1 && numbers[i + j] !== numbers[numbers.length - 1]) ? res += ' ' :
+                (j === cols - 1 && numbers[i + j] !== numbers[numbers.length - 1]) ? res += '\n' : false
         }
-
     }
     return res;
 }
-
-// вариант, когда все столбцы одной длины (длины самого длинного числа)
-// function maxNumLength(numbers) {
-//     let res = 0;
-//     for (let i = 0; i < numbers.length; ++i) {
-//         if (res < String(numbers[i]).length) {
-//             res = String(numbers[i]).length;
-//         }
-//     }
-//     return res;
-// }
-//
-// function format(numbers, cols) {
-//     let res = "";
-//     let maxLength = maxNumLength(numbers);
-//
-//     for (let i = 0; i < numbers.length; i += cols) {
-//         for (let j = 0; j < cols && (i + j < numbers.length); ++j) {
-//             let spaces = "";
-//             let spacesCount = maxLength - String(numbers[i + j]).length;
-//             for (let k = 0; k < spacesCount; ++k) {
-//                 spaces += " ";
-//             }
-//             res += (spaces + numbers[i + j]);
-//         }
-//         if(i < numbers.length-1){
-//             res+='\n';
-//         }
-//     }
-//     return res;
-// }
