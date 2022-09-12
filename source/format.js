9be6ecbd66cf5c407d7c33a84c224dcb0b269815
aralary <makarov.0205@mail.ru>
2022-09-12 20:37:18 +0300

    + use Math.max (if all cols are same width)

--- source/format.js
+++ source/format.js
@@ -1,21 +1,11 @@
 "use strict"
 
-function maxColWidth(numbers, col, colsNumber) {
-    let res = 0;
-    for (let i = col; i < numbers.length; i += colsNumber) {
-        if (res < String(numbers[i]).length) {
-            res = String(numbers[i]).length;
-        }
-    }
-    return res;
-}
-
 let format = (numbers, cols) => {
     let res = "";
+    let maxWidth = Math.max(...(numbers.map(el => String(el).length)));
     for (let i = 0; i < numbers.length; i += cols) {
         for (let j = 0; (j < cols) && (i + j < numbers.length); ++j) {
-            let colWidth = maxColWidth(numbers, j, cols);
-            res += (" ".repeat(colWidth - String(numbers[i + j]).length) + numbers[i + j]);
+            res += (" ".repeat(maxWidth - String(numbers[i + j]).length) + numbers[i + j]);
             (j < cols - 1 && numbers[i + j] !== numbers[numbers.length - 1]) ? res += ' ' :
                 (j === cols - 1 && numbers[i + j] !== numbers[numbers.length - 1]) ? res += '\n' : false
         }
