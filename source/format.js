aa99368a3772449b910659d9644b4e39986ea298
aralary <makarov.0205@mail.ru>
2022-09-12 21:32:37 +0300

    + use Math.max (if cols are not the same width)

--- source/format.js
+++ source/format.js
@@ -1,11 +1,23 @@
 "use strict"
 
+function colsWidthArr(numbers, colsCount) {
+    let res = [];
+    for (let i = 0; i < colsCount; ++i) {
+        let tempCol = [];
+        for (let j = i; j < numbers.length; j += colsCount) {
+            tempCol.push(numbers[j]);
+        }
+        res[i] = Math.max(...(tempCol.map(el => String(el).length)));
+    }
+    return res;
+}
+
 let format = (numbers, cols) => {
     let res = "";
-    let maxWidth = Math.max(...(numbers.map(el => String(el).length)));
+    let widths = colsWidthArr(numbers, cols);
     for (let i = 0; i < numbers.length; i += cols) {
         for (let j = 0; (j < cols) && (i + j < numbers.length); ++j) {
-            res += (" ".repeat(maxWidth - String(numbers[i + j]).length) + numbers[i + j]);
+            res += (" ".repeat(widths[j] - String(numbers[i + j]).length) + numbers[i + j]);
             (j < cols - 1 && numbers[i + j] !== numbers[numbers.length - 1]) ? res += ' ' :
                 (j === cols - 1 && numbers[i + j] !== numbers[numbers.length - 1]) ? res += '\n' : false
         }
