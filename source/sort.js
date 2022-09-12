7ef709d5a0741b09503c071bea3ce23b0879fd4d
zenehu <zeronethunter2001@gmail.com>
2022-09-12 21:45:27 +0300

    `quickSort` func added, some sort of function cleaning

--- source/sort.js
+++ source/sort.js
@@ -1,6 +1,28 @@
 'use strict';
 
 /**
+ * Quick sort implementation.
+ * @param {Array} arr - Array to sort.
+ * @param {Function} compare - Comparator to use.
+ * @return {Array} Sorted array.
+ */
+const quickSort = (arr, compare = (left, right) => { return left > right }) => {
+    if (arr.length < 2) return arr;
+    let pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (compare(pivot, arr[i]) > 0) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left, compare).concat(pivot, quickSort(right, compare));
+}
+
+/**
  * Sort words in sentence and letters in words with first one in uppercase.
  * @param {string} str - String to sort.
  * @return {string} Sorted string.
@@ -12,11 +34,12 @@ const sort = (str) => {
     }
 
     const collator = new Intl.Collator()
-    const words = str.toLowerCase().split(' ');
-    words.forEach((word, index) => {
-            word = word.split('').sort(collator.compare).join('');
-            words[index] = word[0].toUpperCase() + word.slice(1);
-        }
-    )
-    return words.sort(collator.compare).join(' ')
+    const words = str.toLowerCase().split(' ').sort();
+
+    for (let i = 0; i < words.length; i++) {
+        let word = quickSort(words[i].split(''), collator.compare).join('');
+        words[i] = word[0].toUpperCase() + word.slice(1);
+    }
+
+    return quickSort(words, collator.compare).join(' ')
 }
