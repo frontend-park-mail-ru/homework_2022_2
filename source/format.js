155c43845a1c53cdbb3d0557259302e1eef49c36
aralary <makarov.0205@mail.ru>
2022-09-12 23:20:38 +0300

    + tests and exceptions

--- source/format.js
+++ source/format.js
@@ -5,6 +5,10 @@ function colsWidthArr(numbers, colsCount) {
     for (let i = 0; i < colsCount; ++i) {
         let tempCol = [];
         for (let j = i; j < numbers.length; j += colsCount) {
+            numbers[j] = +numbers[j];
+            if (!isFinite(numbers[j])) {
+                throw TypeError("its not a numbers");
+            }
             tempCol.push(numbers[j]);
         }
         res[i] = Math.max(...(tempCol.map(el => String(el).length)));
@@ -13,14 +17,18 @@ function colsWidthArr(numbers, colsCount) {
 }
 
 let format = (numbers, cols) => {
-    let res = "";
-    let widths = colsWidthArr(numbers, cols);
-    for (let i = 0; i < numbers.length; i += cols) {
-        for (let j = 0; (j < cols) && (i + j < numbers.length); ++j) {
-            res += (" ".repeat(widths[j] - String(numbers[i + j]).length) + numbers[i + j]);
-            (j < cols - 1 && numbers[i + j] !== numbers[numbers.length - 1]) ? res += ' ' :
-                (j === cols - 1 && numbers[i + j] !== numbers[numbers.length - 1]) ? res += '\n' : false
+    try {
+        let res = "";
+        const widths = colsWidthArr(numbers, cols);
+        for (let i = 0; i < numbers.length; i += cols) {
+            for (let j = 0; (j < cols) && (i + j < numbers.length); ++j) {
+                res += (" ".repeat(widths[j] - String(numbers[i + j]).length) + numbers[i + j]);
+                (j < cols - 1 && i + j !== numbers.length - 1) ? res += ' ' :
+                    (j === cols - 1 && i + j !== numbers.length - 1) ? res += '\n' : false;
+            }
         }
+        return res;
+    } catch (e) {
+        throw e;
     }
-    return res;
 }
--- tests/format.js
+++ tests/format.js
@@ -1,54 +1,78 @@
 'use strict';
 
 QUnit.module('Тестируем функцию format', function () {
-	QUnit.test('format работает правильно c одной колонкой и положительными числами', function (assert) {
-		const input = [ 0, 1, 2, 10, 100, 1000, 10000 ];
+    QUnit.test('format работает правильно c одной колонкой и положительными числами', function (assert) {
+        const input = [0, 1, 2, 10, 100, 1000, 10000];
 
-		const expected =
-			'    0\n' +
-			'    1\n' +
-			'    2\n' +
-			'   10\n' +
-			'  100\n' +
-			' 1000\n' +
-			'10000';
+        const expected =
+            '    0\n' +
+            '    1\n' +
+            '    2\n' +
+            '   10\n' +
+            '  100\n' +
+            ' 1000\n' +
+            '10000';
 
-		assert.strictEqual(format(input, 1), expected);
-	});
+        assert.strictEqual(format(input, 1), expected);
+    });
 
-	QUnit.test('format работает правильно c одной колонкой и числами разного знака', function (assert) {
-		const input = [ 0, 1, 2, 10, 100, -100, 1000, 10000, -10000 ];
+    QUnit.test('format работает правильно c одной колонкой и числами разного знака', function (assert) {
+        const input = [0, 1, 2, 10, 100, -100, 1000, 10000, -10000];
 
-		const expected =
-			'     0\n' +
-			'     1\n' +
-			'     2\n' +
-			'    10\n' +
-			'   100\n' +
-			'  -100\n' +
-			'  1000\n' +
-			' 10000\n' +
-			'-10000';
+        const expected =
+            '     0\n' +
+            '     1\n' +
+            '     2\n' +
+            '    10\n' +
+            '   100\n' +
+            '  -100\n' +
+            '  1000\n' +
+            ' 10000\n' +
+            '-10000';
 
-		assert.strictEqual(format(input, 1), expected);
-	});
+        assert.strictEqual(format(input, 1), expected);
+    });
 
-	QUnit.test('format работает правильно c несколькими колонками', function (assert) {
-		const input = [ 0, 1, 2, 10, 100, -100, 1000, 10000, -10000 ];
+    QUnit.test('format работает правильно c несколькими колонками', function (assert) {
+        const input = [0, 1, 2, 10, 100, -100, 1000, 10000, -10000];
 
-		const expected2 =
-			'     0     1\n' +
-			'     2    10\n' +
-			'   100  -100\n' +
-			'  1000 10000\n' +
-			'-10000';
+        const expected2 =
+            '     0     1\n' +
+            '     2    10\n' +
+            '   100  -100\n' +
+            '  1000 10000\n' +
+            '-10000';
 
-		const expected3 =
-			'   0     1      2\n' +
-			'  10   100   -100\n' +
-			'1000 10000 -10000';
+        const expected3 =
+            '   0     1      2\n' +
+            '  10   100   -100\n' +
+            '1000 10000 -10000';
 
-		assert.strictEqual(format(input, 2), expected2);
-		assert.strictEqual(format(input, 3), expected3);
-	});
+        assert.strictEqual(format(input, 2), expected2);
+        assert.strictEqual(format(input, 3), expected3);
+    });
+
+    QUnit.test('format работает правильно и выкидывает исключения при наличии не числа в массиве', function (assert) {
+        const input1 = [0, 1, 2, "str", 100, -100, 1000, 10000];
+        assert.throws(() => format(input1, 2), TypeError("its not a numbers"));
+
+        const input2 = [0, 1, 2, [1,2,3,4], 100, -100, 1000, 10000];
+        assert.throws(() => format(input2, 2), TypeError("its not a numbers"));
+
+        const input3 = [0, 1, 2, "w11w", 100, -100, 1000, 10000];
+        assert.throws(() => format(input3, 2), TypeError("its not a numbers"));
+
+        const input4 = [0, 1, 2, "  10  ", 100, -100, 1000, 10000, -10000];
+
+                const expected =
+            '     0     1\n' +
+            '     2    10\n' +
+            '   100  -100\n' +
+            '  1000 10000\n' +
+            '-10000';
+        assert.strictEqual(format(input4, 2), expected);
+
+        const input5 = [0, 1, 2, "11 1", 100, -100, 1000, 10000];
+        assert.throws(() => format(input5, 2), TypeError("its not a numbers"));
+    });
 });
